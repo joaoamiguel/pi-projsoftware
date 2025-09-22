84c88ee5de4b6fcbc9725827dd8f6a525cd0176b
jota <159027699+joaoamiguel@users.noreply.github.com>
2025-09-22 04:27:22 -0300

    Add files via upload

---
+++ pi-projsoftware/pi-projsoftware/src/main/java/com/example/pi_projsoftware/PiProjsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.pi_projsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiProjsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiProjsoftwareApplication.class, args);
+	}
+
+}
